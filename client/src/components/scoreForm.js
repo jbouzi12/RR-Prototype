62568e9d39618ec353d69dc479235d2574e79d54
Jensen Bouzi <jensen.bouzi@gmail.com>
2019-03-23 16:34:23 -0400

    Separate rating form into separate component

---
+++ client/src/components/scoreForm.js
@@ -0,0 +1,103 @@
+import React  from 'react';
+// import { useMutation } from 'react-apollo-hooks';
+import { graphql, Mutation } from 'react-apollo';
+import { View, Text } from 'react-native';
+import { Slider, Button } from 'react-native-elements';
+import {
+  NEW_SCORE,
+  UPDATE_SCORE,
+  SCORE_QUERY
+} from '../queries/scores';
+let scores = [
+    {
+      name: "Flow"
+    },
+    {
+      name: "Delivery"
+    },
+    {
+      name: "Metaphor"
+    },
+    {
+      name: "Adlib"
+    },
+    {
+      name: "Beats"
+    }
+
+  ]
+;
+
+
+// <ScoreForm user={user} artist={artist} />
+const ScoreForm = ({ user, artist }) => {
+    // amount, category, name, email
+    // const submitNewScore = useMutation(NEW_SCORE);
+    // const [isSubmitted, setIsSubmitted] = useState(false);
+
+    return scores.map((score, index) => (
+        <View
+          key={index}
+          style={{
+            marginBottom: 15,
+            alignItems: "center",
+            width: "50%"
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "bold",
+              backgroundColor: "#ddd",
+              padding: 5,
+              color: "#FF365D"
+            }}
+          >
+            {score.name}
+          </Text>
+          <Mutation mutation={NEW_SCORE}>
+            {(submitNewScore) => (
+              <Slider
+                value={1}
+                onValueChange={(value) => {
+                  submitNewScore({
+                    variables: {
+                      amount: value,
+                      category: score.name,
+                      name: artist.name,
+                      email: user && user.email ? user.email : null
+                    }
+                  })
+                  .then(({ data }) => {
+                    console.log('got data', data);
+                  })
+                  .catch((error) => {
+                    console.log('there was an error sending the query', error);
+                  })
+                }}
+                maximumValue = {5}
+                step={1}
+                style={{
+                  width: 120
+                }}
+                minimumTrackTintColor="#FF365D"
+                thumbTintColor= "#ddd"
+              />
+            )}
+          </Mutation>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: "center"
+            }}
+          >
+            <Text > Value: </Text>
+            <Text style={{ color: "#FF365D" }} >
+              {score.name.toLowerCase()}
+            </Text>
+          </View>
+        </View>
+      )
+    )
+};
+
+export default ScoreForm;
---
+++ client/src/queries/scores.js
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag';
+
+export const NEW_SCORE = gql`
+  mutation newScore($amount: Int!, $category: String!, $name: String!, $email: String!) {
+    newScore(amount: $amount, category: $category, name: $name, email: $email) {
+      amount
+    }
+  }
+`
+
+export const UPDATE_SCORE = gql`
+  mutation updateScore($amount: Int!, $category: String!, $name: String!, $email: String!) {
+    updateScore(amount: $amount, category: $category, name: $name, email: $email) {
+      amount
+    }
+  }
+`
+
+export const SCORE_QUERY = gql`
+  query scores($name: String!, $email: String!) {
+    score(name: $name, email: $email) {
+      amount
+    }
+  }
+`
